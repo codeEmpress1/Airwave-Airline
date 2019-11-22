225c05ee7ad993948a2e060eda51a33883df2194
toby <kabiotobiano@gmail.com>
2019-11-22 11:09:15 +0100

    time and date updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import apology, login_required
+from helpers import apology, login_required, send_confirmation_email
 from flask_cors import CORS, cross_origin
 
 
