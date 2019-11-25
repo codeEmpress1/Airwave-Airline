b4fc0993b4310966a27f99cafedda07c59fbcada
toby <kabiotobiano@gmail.com>
2019-11-25 16:41:57 +0100

    merged

--- app.py
+++ app.py
@@ -399,9 +399,7 @@ def addprice():
     return render_template('addprices.html')
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run()
